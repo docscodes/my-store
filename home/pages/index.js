5257393accb1b4b30d897b69c3dd413fe8d22bf2
docscodes <docscodes@gmail.com>
2024-10-03 17:41:40 -0400

    fix format

--- home/pages/index.js
+++ home/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,12 +12,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <nav><a href="/">Home</a> | <a href="/catalog">Catalog</a> </nav>
-    <h1 className={styles.title}>
-          Home:Hello World! 
-        </h1>
+        <nav>
+          <a href="/">Home</a> | <a href="/catalog">Catalog</a>{" "}
+        </nav>
+        <h1 className={styles.title}>Home:Hello World!</h1>
         <h2>Welcome to my store</h2>
-</main>
+      </main>
     </div>
-  )
+  );
 }
